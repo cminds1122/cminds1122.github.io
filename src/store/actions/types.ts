edbd145cf3fcb3a8f4f006a45ccf696e9841c0f2
ved prakash <cminds1122@gmail.com>
2024-09-06 17:16:05 +0530

    Removed source code

--- src/store/actions/types.ts
+++
@@ -1,4 +0,0 @@
-// actions/types.ts
-export const YOUR_ACTION_TYPE = 'YOUR_ACTION_TYPE';
-
-// Define other action types
--- src/store/actions/yourActions.ts
+++
@@ -1,9 +0,0 @@
-// actions/yourActions.ts
-import { Dispatch } from 'redux';
-import { YOUR_ACTION_TYPE } from './types';
-
-export const yourAction = () => (dispatch: Dispatch) => {
-  dispatch({ type: YOUR_ACTION_TYPE });
-};
-
-// Define other action creators
